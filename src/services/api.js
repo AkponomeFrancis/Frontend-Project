fbb9441858d65473d4379172e88b1bf7a596bfca
Akponome Francis <francisfuture080@gmail.com>
2025-06-25 22:20:26 +0100

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://backend-project-pqf2.onrender.com' });
+const API = axios.create({ baseURL: 'https://backend-project-pqf2.onrender.com/api' });
 
 // const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
